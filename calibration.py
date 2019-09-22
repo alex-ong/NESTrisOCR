678255e6f8c394c82a58665111aad05afe86e82f
alex-ong <the.onga@gmail.com>
2019-09-22 17:10:01 +1000

    Moved calibiration into config.ini

--- calibration.py
+++ calibration.py
@@ -1,9 +1,13 @@
+import configparser
+from ast import literal_eval #safe version of eval
 
-WINDOW_NAME = "OBS"    
-CAPTURE_COORDS = (27,100,611,505)
-scorePerc = (0.754,0.249,0.188,0.033)
-linesPerc = (0.596,0.069,0.092,0.033)
-levelPerc = (0.818,0.714,0.060,0.032)
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-statsPerc  = (0.187,0.399,0.091,0.434)
-stats2Perc = (0.469,0.175,0.119,0.060)
+WINDOW_NAME = config['calibration']['window_name']
+CAPTURE_COORDS = literal_eval(config['calibration']['game_coords'])
+scorePerc = literal_eval(config['calibration']['scorePerc'])
+linesPerc = literal_eval(config['calibration']['linesPerc'])
+levelPerc = literal_eval(config['calibration']['levelPerc'])
+statsPerc  = literal_eval(config['calibration']['statsPerc'])
+stats2Perc = literal_eval(config['calibration']['stats2Perc'])
