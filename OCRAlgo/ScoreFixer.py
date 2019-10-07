43a5ebd4212f148fdf52b010d2331afc9fde40f7
alex-ong <the.onga@gmail.com>
2019-10-07 21:46:51 +1100

    Added 4/A discernation in ScoreFixer.py

--- OCRAlgo/ScoreFixer.py
+++ OCRAlgo/ScoreFixer.py
@@ -24,6 +24,14 @@ class ScoreFixer(object):
             if stringNumber[0] == 'B':
                 stringNumber = '8' + stringNumber[1:]
         
+        #switch first digit between 4 and A dependant on last state.
+        if self.lastGoodValue[0] == '9' or self.lastGoodValue[0] == 'A':
+            if stringNumber[0] == '4':
+                stringNumber = 'A' + stringNumber[1:]
+        
+        if self.lastGoodValue[0] == '3' or self.lastGoodValue[0] == '4':
+            if stringNumber[0] == 'A':
+                stringNumber = '4' + stringNumber[1:]
         self.lastGoodValue = stringNumber
         
         return stringNumber
