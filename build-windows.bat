2457417a0361bc1b806587b16527f553ea0a7961
Alex Ong <alex.ong@unsw.edu.au>
2019-10-09 20:17:12 +1100

    Added multiple build options from spec files.

--- build-windows.bat
+++ build-windows.bat
@@ -1,3 +1,16 @@
+rem We have committed the built numba plugins, so we shouldn't need to rebuild. Uncomment these lines to rebuild
+rem You'll need MSVC 14.0 to build, as well as numba.
+
+rem first, build the numba plugins
+rem cd Networking
+rem python buildByteStuffer2.py
+rem cd ../OCRAlgo
+rem python buildBoardOCR2.py
+
+rem the following spec file assumes we don't need llvmlite since we've excluded numba.
 pyinstaller -D main.spec
+
+rem the following spec file assumes we need llvmlite
+rem pyinstaller -D main-llvmlite.spec
 
 
