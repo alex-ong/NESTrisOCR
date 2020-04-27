17744e03de49432741f931c4cccd643d0159e067
alex-ong <the.onga@gmail.com>
2020-04-27 20:05:50 +1000

    Remove some build requirements
    Removed black specific version requirement. This should use your local python (3.6+)
    Removed simpleaudio wheel (the developer released a wheel for 3.7 and 3.8)

--- scripts/install-requirements.bat
+++ scripts/install-requirements.bat
@@ -1,6 +1,2 @@
-REM simpleaudio currently needs to be compiled.
-REM We've prebuilt a version here.
-pip3 install wheel/simpleaudio-1.0.2-cp37-cp37m-win32.whl
-
 REM here we install everything else
 pip3 install -r ../requirements.txt
