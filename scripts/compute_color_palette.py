5dfbc31628b3753a06944dd38c78789dceb15638
Timothee Groleau <tgroleau@zendesk.com>
2020-05-18 18:39:35 +0800

    Capture areas from nes logical pixels, and update easiercap palette accordingly

--- scripts/compute_color_palette.py
+++ scripts/compute_color_palette.py
@@ -1,23 +1,43 @@
 # invoke from root as python3 -m scripts.compute_color_palette easiercap nestris_ocr/assets/sample_inputs/easiercap/lvl%d/field.png
 import json
 import sys
-from PIL import Image, ImageFilter
+from PIL import Image
 import numpy as np
 from nestris_ocr.colors import Colors
-from math import sqrt
+from math import sqrt, floor
+
+
+def getAverageColor(pixels):
+    return (
+        round(sqrt(sum([p[0] * p[0] for p in pixels]) / len(pixels))),
+        round(sqrt(sum([p[1] * p[1] for p in pixels]) / len(pixels))),
+        round(sqrt(sum([p[2] * p[2] for p in pixels]) / len(pixels))),
+    )
 
 
 def getColors(level, field):
     img = Image.open(field)
 
-    # extra smoothness from PIL
-    slight_blur = ImageFilter.GaussianBlur()
-    slight_blur.radius = 1
+    res_img = Image.new("RGBA", (img.width, img.height), (0, 0, 0, 255),)
 
-    img = img.filter(slight_blur)
+    res_img.paste(img.convert("RGBA"), (img.width * 0, 0))
 
-    spanx = img.width / 10
-    spany = img.height / 20
+    # nes pix size, block span and capture are only valid on perfect calibration
+    # perfect calibration implies capturing the right and bottom black edges
+    # still, since we're capturing a safe zone, things should still work with small discrepancies
+
+    nes_pix_xsize = img.width / 80
+    nes_pix_ysize = img.height / 160
+
+    spanx = nes_pix_xsize * 8
+    spany = nes_pix_ysize * 8
+
+    # floor() to err on size of caution and not accidentally capture edges
+    capx = floor(nes_pix_xsize * 2)
+    capy = floor(nes_pix_ysize * 2)
+
+    corner_highlight = Image.new("RGBA", (2, 2), (255, 0, 0, 128))
+    capture_highlight = Image.new("RGBA", (capx, capy), (0, 255, 0, 128))
 
     np_img = np.array(img, dtype=np.uint16)
 
@@ -28,41 +48,38 @@ def getColors(level, field):
 
     for y in range(20):
         for x in range(10):
-            xidx = round(spanx * (x + 0.5))
-            yidx = round(spany * (y + 0.5))
+            xidx = round(spanx * x)
+            yidx = round(spany * y)
 
-            pix = [0, 0, 0]
+            # print the top-left corner
+            res_img.paste(corner_highlight, (xidx, yidx), corner_highlight)
 
-            # grab 9 pixels in a 3x3 square for each block
-            # on the lower-right quadrant
-            # and compute color average
-            for i in range(xidx, xidx + 3):
-                for j in range(yidx, yidx + 3):
-                    tmp = np_img[j, i]
-                    pix[0] += tmp[0] * tmp[0]
-                    pix[1] += tmp[1] * tmp[1]
-                    pix[2] += tmp[2] * tmp[2]
+            xidx = round(
+                spanx * x + nes_pix_xsize * 3.5
+            )  # 3.5 to favour capturing on the right
+            yidx = round(spany * y + nes_pix_xsize * 3)
 
-            pix[0] = sqrt(pix[0] / 9)
-            pix[1] = sqrt(pix[1] / 9)
-            pix[2] = sqrt(pix[2] / 9)
+            res_img.paste(capture_highlight, (xidx, yidx), capture_highlight)
+
+            pixels = []
+
+            # grab all pixels in the capture area
+            for i in range(xidx, xidx + capx):
+                for j in range(yidx, yidx + capy):
+                    pixels.append(np_img[j, i])
+
+            pix = getAverageColor(pixels)
 
             col_index = colors.getClosestColorIndex(pix)
 
-            color_instances[col_index].append(pix)
+            color_instances[col_index].extend(pixels)
+
+    if level == 0:
+        res_img.convert("RGB").resize((img.width * 3, img.height * 3)).show()
 
     # average the colors
-    col1 = (
-        round(sum([c[0] for c in color_instances[2]]) / len(color_instances[2])),
-        round(sum([c[1] for c in color_instances[2]]) / len(color_instances[2])),
-        round(sum([c[2] for c in color_instances[2]]) / len(color_instances[2])),
-    )
-
-    col2 = (
-        round(sum([c[0] for c in color_instances[3]]) / len(color_instances[3])),
-        round(sum([c[1] for c in color_instances[3]]) / len(color_instances[3])),
-        round(sum([c[2] for c in color_instances[3]]) / len(color_instances[3])),
-    )
+    col1 = getAverageColor(color_instances[2])
+    col2 = getAverageColor(color_instances[3])
 
     return col1, col2
 
@@ -127,6 +144,7 @@ if __name__ == "__main__":
 
     palette_img.save("nestris_ocr/palettes/%s.png" % (palette_name,))
 
-    # show both reference palette and computed palette for comparison
     palette_img.show()
-    getPaletteImage(colors.palette).show()
+
+    # shows the default palette for comparison
+    # getPaletteImage(colors.palette).show()
