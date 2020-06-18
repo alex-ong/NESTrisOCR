7380f0a979efa1f541e2790ef29f148356cbdc1a
alex-ong <the.onga@gmail.com>
2020-06-18 22:28:32 +1000

    Change FileCapture to not throw exception during constructor.
    When file doesnt exist, it simply returns. This is now handled by a smooth switch to null capture.

--- nestris_ocr/capturing/file.py
+++ nestris_ocr/capturing/file.py
@@ -5,22 +5,28 @@ from typing import Tuple
 from nestris_ocr.capturing.base import AbstractCapture
 from nestris_ocr.utils import xywh_to_ltrb
 from nestris_ocr.types import XYWHBox
+from os.path import isfile
 
 
 class FileCapture(AbstractCapture):
     def __init__(self, source_id: str, xywh_box: XYWHBox, extra_data: str) -> None:
         super().__init__(source_id, xywh_box, extra_data)
 
-        self.cap = cv2.VideoCapture(source_id)
-        if not self.cap.isOpened():
-            raise Exception(f"Wrong file: {source_id}")
+        if not isfile(source_id):
+            print(f"File does not exist: {source_id}")
+            self.cap = None
+            return
 
+        self.cap = cv2.VideoCapture(source_id)
+        self.cur_frame = 0
         self.frame_duration = 1.0 / self.cap.get(cv2.CAP_PROP_FPS)
         self.total_frames = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
 
-        self.cur_frame = 0
-
     def get_image(self, rgb: bool = False) -> Tuple[float, Image.Image]:
+
+        if not self.cap:
+            return None, None
+
         self.cur_frame += 1
         cv2_retval, cv2_image = self.cap.read()
 
