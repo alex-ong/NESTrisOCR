da84784c65ba7981a0b4cf311da3e9266915caf8
Alex Ong <the.onga@gmail.com>
2020-07-12 11:14:38 +1000

    fix: debug_print was causing double-prints and has been removed

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from calibrate import mainLoop as calibrateLoop
 from nestris_ocr.capturing import uncached_capture
 
 # from nestris_ocr.scan_strat.fastest_strategy import FastestStrategy as Strategy
-
 from nestris_ocr.scan_strat.naive_strategy import NaiveStrategy as Strategy
 from nestris_ocr.network.network_client import NetClient
 
@@ -16,11 +15,6 @@ from nestris_ocr.config import config
 from nestris_ocr.utils.program_args import args
 
 RATE = 1.0 / config["performance.scan_rate"]
-
-
-def debug_print(msg):
-    if config["debug.print_packet"]:
-        print(msg)
 
 
 def main(on_cap, check_network_close):
@@ -33,7 +27,6 @@ def main(on_cap, check_network_close):
             ts, image = uncached_capture().get_image(rgb=True)
 
             if not ts and not image:
-                debug_print("Exiting cleanly")
                 break
         except KeyboardInterrupt:
             break
@@ -52,8 +45,6 @@ def main(on_cap, check_network_close):
             print(f"Elapsed time since capture: {elapsed_time}")
             strategy_time = time.time() - pre_strategy_ts
             print(f"Strategy processing time: {strategy_time}")
-
-        debug_print(result)
 
         on_cap(result, ts)
 
