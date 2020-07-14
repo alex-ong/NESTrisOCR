8f7b5dfcad24ec144384cd83ca40edecfbfcba52
Timothee Groleau <timothee.groleau@gmail.com>
2020-07-14 23:28:03 +0800

    restream.py: nicer way to handle the template for

--- scripts/restream.py
+++ scripts/restream.py
@@ -1,12 +1,11 @@
-# invoke from root as python -m scripts.restream
 import sys
 from subprocess import Popen, PIPE
 import time
 import re
+import json
 
-with open("scripts/config.stream.template.json", "r") as content_file:
-    config_base_file = content_file.read()
 
+TEMPLATE_FILE = "scripts/config.stream.template.json"
 
 CAP_RATIOS = [1233 / 1920, 1]  # based on the stencil dimensions for 1080p
 
@@ -18,10 +17,10 @@ PLAYER_SETTINGS = [
 
 
 def setupPlayer(twitchName, playerNum):
-    local_stream_port, ocr_dest_port = PLAYER_SETTINGS[playerNum - 1]
+    localStreamPort, ocrDestPort = PLAYER_SETTINGS[playerNum - 1]
 
     twitchUrl = "twitch.tv/{}".format(twitchName)
-    localUrl = "http://localhost:{}".format(local_stream_port)
+    localUrl = "http://localhost:{}".format(localStreamPort)
 
     fps = -1
 
@@ -35,7 +34,8 @@ def setupPlayer(twitchName, playerNum):
         "1080p60",
     ]
 
-    # run local restreamer over http
+    # ==================================
+    # 1. run local restreamer over http
     Popen(
         [
             "streamlink",
@@ -43,20 +43,22 @@ def setupPlayer(twitchName, playerNum):
             ",".join(resolutions),
             "--player",
             "vlc --intf dummy --sout '#standard{access=http,mux=mkv,dst=localhost:"
-            + str(local_stream_port)
+            + str(localStreamPort)
             + "}'",
         ]
     )
 
     time.sleep(10)
 
+    # ==================================
+    # 2. read stream details with ffmpeg
     p = Popen(["ffmpeg", "-i", localUrl], stdout=PIPE, stderr=PIPE)
 
     stdout, stderr = p.communicate()
 
-    fps_re = re.compile(", (?P<fps>[0-9.]+) tbr,")
+    fpsRegex = re.compile(", (?P<fps>[0-9.]+) tbr,")
 
-    m = fps_re.search(str(stderr))
+    m = fpsRegex.search(str(stderr))
 
     try:
         fps = int(m.group("fps"))
@@ -65,38 +67,49 @@ def setupPlayer(twitchName, playerNum):
 
     print("Found fps", fps)
 
-    size_re = re.compile(", (?P<width>\\d+)x(?P<height>\\d+)(,| \\[SAR)")
+    sizeRegex = re.compile(", (?P<width>\\d+)x(?P<height>\\d+)(,| \\[SAR)")
 
-    m = size_re.search(str(stderr))
+    m = sizeRegex.search(str(stderr))
 
     width = int(m.group("width"))
     height = int(m.group("height"))
 
     print("Found size", width, height)
 
-    # Update player config file
-    player_config = (
-        config_base_file.replace("USER_NAME", twitchName)
-        .replace("twitchName", twitchName)
-        .replace("twitchUrl", twitchUrl)
-        .replace("STREAM_URL", localUrl)
-        .replace("PORT", str(ocr_dest_port))
-        .replace("SCAN_RATE", str(fps))
-        .replace("CAP_WIDTH", str(round(width * CAP_RATIOS[0])))
-        .replace("CAP_HEIGHT", str(round(height * CAP_RATIOS[1])))
-    )  # TODO read from ffmpeg instead
+    # ==================================
+    # 3. write player config file from template
 
-    player_config_filename = "config.competition.p{}.json".format(playerNum)
+    # Not using the Config class because it requires CLI args
+    # which are too much for this tiny script
 
-    with open(player_config_filename, "w") as content_file:
-        content_file.write(player_config)
+    playerConfigFile = "config.competition.p{}.json".format(playerNum)
 
-    # Run calibrator on player stream
-    p = Popen(["python3", "calibrate.py", "--config", player_config_filename])
+    with open(TEMPLATE_FILE) as jsonFile:
+        config = json.load(jsonFile)
+
+    config["player.name"] = twitchName
+    config["player.twitch_url"] = twitchUrl
+    config["performance.scan_rate"] = fps
+    config["calibration.game_coords"] = [
+        0,
+        0,
+        round(width * CAP_RATIOS[0]),
+        round(height * CAP_RATIOS[1]),
+    ]
+    config["network.port"] = ocrDestPort
+    config["capture.source_id"] = localUrl
+
+    with open(playerConfigFile, "w") as jsonFile:
+        json.dump(config, jsonFile)
+
+    # ==================================
+    # 4. Run calibrator on player stream
+    p = Popen(["python3", "calibrate.py", "--config", playerConfigFile])
     p.wait()
 
-    # Run NESTrisOCR on player stream
-    Popen(["python3", "main.py", "--config", player_config_filename])
+    # ==================================
+    # 5. Run NESTrisOCR
+    Popen(["python3", "main.py", "--config", playerConfigFile])
 
 
 if __name__ == "__main__":
